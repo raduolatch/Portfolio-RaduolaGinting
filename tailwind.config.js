d0ee84138a89e7a47133dad4320839e414efe782
raduolatch <raduola01@gmail.com>
2026-06-17 00:59:14 +0700

    ignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     extend: {
       colors: {
         primary: 'oklch(68.5% 0.169 237.323)',
+        secondary: 'oklch(55.4% 0.046 257.417)',
         dark : 'oklch(20.8% 0.042 265.755)',
       },
       screens:{'2xl':'1320px'
